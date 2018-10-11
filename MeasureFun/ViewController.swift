0cbabe437280a49b1892dc9c85cd32e79e43db23
YassineDaoudi1 <yassine.daoudi@findl.lu>
2018-10-11 17:36:27 +0200

    MeasureFun

--- MeasureFun/ViewController.swift
+++ MeasureFun/ViewController.swift
@@ -11,8 +11,17 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var targetView: UIView!
+    
+    @IBOutlet weak var measurmentLabel: UILabel!
+    
+    @IBOutlet weak var theButton: UIButton!
+    
+    var firstBox:SCNNode?
+    var secondBox:SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +33,77 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
     }
     
+    @IBAction func measurmentButton(_ sender: Any) {
+        
+        if firstBox == nil {
+            firstBox = addBox()
+            if firstBox != nil {
+                theButton.setTitle("End Point", for: .normal)
+            }
+        }else if secondBox == nil {
+            secondBox = addBox()
+            if secondBox != nil {
+                calcDistance()
+                theButton.setTitle("Reset", for: .normal)
+            }
+        }else {
+            firstBox?.removeFromParentNode()
+            secondBox?.removeFromParentNode()
+            firstBox = nil
+            secondBox = nil
+            measurmentLabel.text = ""
+            theButton.setTitle("Start Point", for: .normal)
+        }
+        
+    }
+    
+    func calcDistance() {
+        if let firstBox = firstBox {
+            if let secondBox = secondBox {
+                let vector = SCNVector3Make(secondBox.position.x - firstBox.position.x, secondBox.position.y - firstBox.position.y, secondBox.position.z - firstBox.position.z)
+                let distance = sqrt(vector.x*vector.x+vector.y*vector.y+vector.z*vector.z)
+                
+                measurmentLabel.text = "\(distance)m"
+            }
+        }
+        
+        
+    }
+    
+    func addBox() -> SCNNode? {
+        
+        let userTouch = sceneView.center
+        
+        let testResults = sceneView.hitTest(userTouch, types: .featurePoint)
+        
+        if let theResult = testResults.first {
+            
+            let box = SCNBox(width: 0.005, height: 0.005, length: 0.005, chamferRadius: 0.005)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.green
+            box.firstMaterial = material
+            let boxNode = SCNNode(geometry: box)
+            boxNode.position = SCNVector3(theResult.worldTransform.columns.3.x,theResult.worldTransform.columns.3.y,theResult.worldTransform.columns.3.z)
+            
+            sceneView.scene.rootNode.addChildNode(boxNode)
+            return boxNode
+        }
+        return nil
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -51,17 +119,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
-    }
-*/
+     return node
+     }
+     */
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
